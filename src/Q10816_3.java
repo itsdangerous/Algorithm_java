759f94381dfb96c87c08ecbe95363e45098d1170
itsdangerous <danger121@naver.com>
2023-02-13 00:27:06 +0900

    refactor: 숫자카드 2 dictionary version - non try-catch, using if-else

--- src/Q10816_3.java
+++ src/Q10816_3.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.StringTokenizer;
-//
 public class Q10816_3 {
     public static void main(String[] args) throws IOException {
 
