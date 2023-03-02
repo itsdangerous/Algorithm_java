160e5e2a4effd3c4729ca082a42d909a512cf3bf
root <root@DESKTOP-DOGVPUB>
2023-03-02 17:50:48 +0900

    solve : swea_1767 프로세서 연결하기

---
+++ src/form_Dijkstra.java
@@ -0,0 +1,108 @@
+import org.w3c.dom.Node;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class form_Dijkstra {
+
+    static final int INF = 9999999;
+    static List<List<Node>> graph; // 그래프를 표현 할 2차원 List
+    static int[] result; // 최단거리 테이블을 표현할 배열
+
+    static boolean[] visited; // 방문처리를 위한 배열
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+        int startIndex = Integer.parseInt(br.readLine());
+
+        //그래프 생성
+        graph = new ArrayList<>();
+        for (int i = 0; i < E; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        // 최단거리 테이블 생성
+        result = new int[E];
+
+        //최단거리테이블 INF로 초기화
+        Arrays.fill(result, INF);
+
+        //방문처리를 위한 배열 생성
+        visited = new boolean[E];
+
+
+        //문제에서 주어진 입력 값에 따른 그래프 초기화
+        for (int i = 0; i < E; i++) {
+           st = new StringTokenizer(br.readLine());
+           int from = Integer.parseInt(st.nextToken());
+           int to = Integer.parseInt(st.nextToken());
+           int weight = Integer.parseInt(st.nextToken());
+            graph.get(from).add(new Node(to, weight));
+
+        }
+
+        //다익스트라 수행
+        dijkstra(startIndex);
+
+        //문제 조건에 맞게 출력
+        for (int i = 1; i < result.length; i++) {
+
+            if (result[i] == INF) {
+                System.out.println(INF);
+            } else {
+                System.out.println(result[i]);
+            }
+        }
+
+    }
+
+    private static void dijkstra(int index) {
+
+        //최단거리가 갱신 된 노드들을 담을 우선순위 큐 생성
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+
+        //최단거리테이블의 시작지점노드 값 0으로 갱신
+        result[index] = 0;
+        pq.offer(new Node(index, 0)); // 우선순위 큐에 시작노드 넣기
+
+        while (!pq.isEmpty()) {
+            Node node = pq.poll();
+            int nodeIndex = node.index;
+            int distance = node.distance;
+
+            if(visited[nodeIndex]) continue;
+            visited[nodeIndex] = true;
+
+            if(distance> result[nodeIndex]) continue;
+            for (Node linkedNode : graph.get(nodeIndex)) {
+                if (distance + linkedNode.distance < result[linkedNode.index]) {
+                    result[linkedNode.index] = distance + linkedNode.distance;
+                    pq.offer(new Node(linkedNode.index, result[linkedNode.index]));
+                }
+            }
+        }
+    }
+
+
+
+    static class Node implements Comparable<Node> {
+
+        int index;
+        int distance;
+
+        public Node(int index, int distance) {
+            this.index = index;
+            this.distance = distance;
+        }
+        @Override
+        public int compareTo(Node o) {
+            return Integer.compare(this.distance, o.distance);
+        }
+    }
+}
---
+++ src/form_prim_MST.java
@@ -0,0 +1,83 @@
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class form_prim_MST {
+    static int V, E;
+
+    static class Edge implements Comparable<Edge> {
+        int v;
+        int w;
+
+        public Edge(int v, int w) {
+            this.v = v;
+            this.w = w;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return Integer.compare(this.w, o.w);
+        }
+    }
+
+    static long min;
+    static int[] distance;
+    static boolean[] checked;
+    static int MM = Integer.MAX_VALUE / 1000;
+    static List<Edge>[] adj;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+        adj = new ArrayList[V + 1];
+        for (int i = 1; i <= V; i++) {
+            adj[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            adj[s].add(new Edge(e, w));
+            adj[e].add(new Edge(s, w));
+        }
+
+        distance = new int[V + 1];
+        checked = new boolean[V + 1];
+        Arrays.fill(distance, MM);
+
+        //간적쿠 간만프
+
+        distance[1] = 0;
+        for (int i = 1; i < V + 1; i++) {
+            int w = -1;
+            int minV = MM;
+            for (int j = 1; j < V + 1; j++) {
+                if (!checked[j] && minV > distance[j]) {
+                    minV = distance[j];
+                    w = j;
+                }
+            }
+            checked[w] = true;
+            min += minV;
+            for (int v = 0; v < adj[w].size(); v++) {
+                Edge edge = adj[w].get(v);
+
+                if (!checked[edge.v] && edge.w < distance[edge.v]) {
+                    distance[edge.v] = edge.w;
+                }
+            }
+        }
+        System.out.println(min);
+
+    }
+}
