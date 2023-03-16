0bcd817ecdea01ac6e3d657d6f4727983b01bdc0
itsdangerous <danger121@naver.com>
2023-03-17 01:14:04 +0900

    0317

--- src/Q2096.java
+++ src/Q2096.java
@@ -1,69 +1,3 @@
-<<<<<<< HEAD
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
-public class Q2096 {
-    static int N;
-    static int[] maxArr, minArr, prevMaxArr, prevMinArr;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        N = Integer.parseInt(br.readLine());
-        maxArr = new int[3];
-        minArr = new int[3];
-        prevMaxArr = new int[3];
-        prevMinArr = new int[3];
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        for (int i = 0; i < 3; i++) {
-            int tmp = Integer.parseInt(st.nextToken());
-            maxArr[i] = tmp;
-            minArr[i] = tmp;
-        }
-
-        for (int i = 1; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-
-            for (int j = 0; j < 3; j++) {
-                prevMaxArr[j] = maxArr[j];
-                prevMinArr[j] = minArr[j];
-                int tmp = Integer.parseInt(st.nextToken());
-                maxArr[j] = tmp;
-                minArr[j] = tmp;
-            }
-            updateMaxArr(prevMaxArr);
-            updateMinArr(prevMinArr);
-        }
-
-        int max = get3Max(maxArr[0], maxArr[1], maxArr[2]);
-        int min = get3Min(minArr[0], minArr[1], minArr[2]);
-        System.out.println(max+" "+min);
-    }
-
-    private static void updateMaxArr(int[] arr) {
-        maxArr[0] += Math.max(arr[0], arr[1]);
-        maxArr[1] += get3Max(arr[0], arr[1], arr[2]);
-        maxArr[2] += Math.max(arr[1], arr[2]);
-    }
-    private static void updateMinArr(int[] arr) {
-        minArr[0] += Math.min(arr[0], arr[1]);
-        minArr[1] += get3Min(arr[0], arr[1], arr[2]);
-        minArr[2] += Math.min(arr[1], arr[2]);
-    }
-
-    private static int get3Max(int a, int b, int c) {
-        return Math.max(Math.max(a, b), c);
-    }
-
-    private static int get3Min(int a, int b, int c) {
-        return Math.min(Math.min(a, b), c);
-    }
-
-}
-=======
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -90,13 +24,6 @@ public class Q2096 {
             minArr[i] = tmp;
         }
 
-        if (N == 1) {
-            int max = get3Max(maxArr[0], maxArr[1], maxArr[2]);
-            int min = get3Min(minArr[0], minArr[1], minArr[2]);
-            System.out.println(max + " " + min);
-            return;
-        }
-
         for (int i = 1; i < N; i++) {
             st = new StringTokenizer(br.readLine());
 
@@ -136,4 +63,3 @@ public class Q2096 {
     }
 
 }
->>>>>>> ee18cbd (12)
--- src/Q25603.java
+++ src/Q25603.java
@@ -1,44 +1,49 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Q25603 {
+    static int N, K;
+    static int[] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
 
-        int[][] arr = new int[N][2];
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            int tmp = Integer.parseInt(st.nextToken());
-            arr[i][0] = tmp;
-            arr[i][1] = i;
-        }
-        int k = 1;
-        for (int i = 1; i < N; i++) {
-            if (arr[i - 1][0] < arr[i][0]) {
-                k++;
+        arr = new int[N];
 
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int startIndex = 0;
+        int max = 0;
+        while (startIndex < N - K+1) {
+            int min = Integer.MAX_VALUE;
+            int index = -1;
+            for (int i = startIndex; i < startIndex + K; i++) {
+                //슬라이스를 startIndex부터 탐색하면서 슬라이스 내의 min값을 갱신
+
+                //만약 min이 이전 max보다 작다면 그것을 선택하고 그것의 인덱스를 시작으로
+
+                // 만약 슬라이스를 모두 돌면서 슬라이스 내의 min값을 갱신 한 후 이전 max보다 작은 게 없다면..
+                // 슬라이스 내의 가장 작은 것을 인덱스로 하고 다시 돌자
+                if (min > arr[i]) {
+                    min = arr[i];
+                    index = i;
+                    if (max > min) {
+                        break;
+                    }
+                }
             }
+            if (max < min) max = min;
+            startIndex = index+1;
+//            System.out.printf("startIndex = %d, index = %d  arr[index] = %d, max = %d\n", startIndex, index, arr[index], max);
         }
-
-        Arrays.sort(arr, Comparator.comparingInt(o -> o[0]));
-        System.out.println(Arrays.deepToString(arr));
-
-        for (int i = 0; i < N; i++) {
-            arr[i][0] = i+1;
-        }
-
-        Arrays.sort(arr, Comparator.comparingInt(o -> o[1]));
-        System.out.println(Arrays.deepToString(arr));
-
-
+        System.out.println(max);
     }
 }
