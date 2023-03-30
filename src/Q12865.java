7ca698e95dbfac396cdacf6fe3bb4ce63ee6a7dd
Kimsumin <ksumin0730@naver.com>
2023-03-30 17:56:21 +0900

    으으응 기모리

---
+++ src/Q12865.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class Q12865 {
+
+    static int[] p;
+    public static void main(String[] args) throws IOException {
+    }
+}
--- src/Test.java
+++ src/Test.java
@@ -1,28 +1,69 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Test {
+
+    static int[] p;
+    static int[] nums;
+    static int N;
+    static int R;
+    static boolean[] visited;
+    static int[][] dist;
+    static int min = Integer.MAX_VALUE;
+
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 
-        int n = 21;
-        int[][] ncr = new int[n][n];
-        for (int m = 1; m < n; m++) {
-            ncr[m][0] = 1;
-            ncr[m][m] = 1;
-            for (int r = 1; r < m; r++) {
-                ncr[m][r] = ncr[m - 1][r - 1] + ncr[m - 1][r];
+        N = Integer.parseInt(br.readLine());
+        visited = new boolean[N];
+        dist = new int[N][N];
+        p = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            p[i] = i;
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                dist[i][j] = Integer.parseInt(st.nextToken());
             }
         }
+        nums = new int[N];
 
-        for (int m = 1; m < n; m++) {
-            for (int r = 0; r < m + 1; r++) {
-                System.out.print(ncr[m][r]+" ");
+        perm(0);
+        System.out.println(min);
+
+
+    }
+
+    static void perm(int cnt) {
+        if (cnt == N) {
+
+            int cost = 0;
+            for (int i = 0; i < N-1; i++) {
+                int c = dist[nums[i]][nums[i+1]];
+                if (c == 0) return;
+                cost += c;
             }
-            System.out.println();
+            int c = dist[nums[N - 1]][nums[0]];
+            if (c==0) return;
+            cost += c;
+
+            min = Math.min(min, cost);
+            return;
         }
 
+        for (int i = 0; i < N; i++) {
+
+            if (visited[i]) continue;
+
+            visited[i] = true;
+            nums[cnt] = p[i];
+            perm(cnt + 1);
+            nums[cnt] = 0;
+            visited[i] =false;
+        }
     }
 }
