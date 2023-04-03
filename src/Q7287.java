34ab1a0d959dd47168d5314a9179106d433adcc1
itsdangerous <danger121@naver.com>
2023-04-03 22:43:04 +0900

    123123123

---
+++ src/Q7287.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+public class Q7287 {
+    public static void main(String[] args) throws IOException {
+        System.out.println(262);
+        System.out.println("valued");
+    }
+
+}
