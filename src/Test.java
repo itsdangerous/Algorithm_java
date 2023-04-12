7a250e1f9a2ba65d6d2012a0653298c6095d5013
itsdangerous <danger121@naver.com>
2023-04-12 09:00:30 +0900

    ㅎㅇ

--- src/Test.java
+++ src/Test.java
@@ -1,209 +1,105 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Test {
-    static final int[][] DIR = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-    static final int EMPTY = -1;
-    static final int ISLAND = -2;
-    static final int GOAL_ISLAND = 0;
-    static final int LENGTH = 1;
-    static int N, M;
-    static int[][] map;
-    static int[] parent;
-    static int numOfIsland = 0;
-    static int[][] dist;
-    static boolean[][] hasBridge;
-    static Map<Integer, List<Point>> islandInfo = new HashMap<>();
 
-    public static void main(String[] args) throws IOException {
+    static int V, E;
+    static int[] p;
+    static int[] rank;
+    static PriorityQueue<Edge> pq;
+    static int answer;
+
+    static class Edge implements Comparable<Edge> {
+
+        int s, e;
+        long w;
+
+        public Edge(int s, int e, long w) {
+            super();
+            this.s = s;
+            this.e = e;
+            this.w = w;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return Long.compare(this.w, o.w);
+        }
+
+    }
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
 
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        map = new int[N][M];
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
-                int element = Integer.parseInt(st.nextToken());
-                if (element == 0) {
-                    map[i][j] = EMPTY;
-                } else {
-                    map[i][j] = ISLAND;
-                }
+        int T = Integer.parseInt(br.readLine());
+
+        for (int TC = 1; TC < T + 1; TC++) {
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            V = Integer.parseInt(br.readLine());
+            E = Integer.parseInt(br.readLine());
+            p = new int[V];
+            rank = new int[V];
+            answer = 0;
+            pq = new PriorityQueue<>();
+            for (int i = 0; i < E; i++) {
+                st = new StringTokenizer(br.readLine());
+                int s = Integer.parseInt(br.readLine());
+                int e = Integer.parseInt(br.readLine());
+                long w = Long.parseLong(br.readLine());
+                pq.offer(new Edge(s, e, w));
+            }
+
+            makeSet();
+            kruskal();
+            System.out.printf("#%d %d\n", TC, answer);
+        }
+
+    }
+
+    static void makeSet() {
+        for (int i = 0; i < V; i++) {
+            p[i] = i;
+            rank[i] = 1;
+        }
+    }
+
+    static void kruskal() {
+
+        int cnt = 0;
+
+        while (cnt != V - 1) {
+            Edge edge = pq.poll();
+            if (union(edge.s, edge.e)) {
+                answer += edge.w;
             }
         }
 
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (map[i][j] == ISLAND) {
-                    islandInfo.put(numOfIsland, new ArrayList<>());
-                    bfs(i, j, numOfIsland++);
-                }
-            }
-        }
-
-        dist = new int[numOfIsland][numOfIsland];
-        hasBridge = new boolean[numOfIsland][numOfIsland];
-        initParent();
-        initDist();
-
-        for (int i=0; i<numOfIsland; i++) {
-            updateDistance(i);
-        }
-
-        PriorityQueue<Bridge> pq = new PriorityQueue<>();
-        for (int i=0; i<numOfIsland; i++) {
-            for (int j=i+1; j<numOfIsland; j++) {
-                if (hasBridge[i][j]) {
-                    pq.add(new Bridge(i, j));
-                }
-            }
-        }
-
-
-        int connectedIsland = 1;
-        int totalLength = 0;
-        while (!pq.isEmpty() && connectedIsland < numOfIsland) {
-            Bridge bridge = pq.poll();
-            if (union(bridge.v1, bridge.v2)) {
-                connectedIsland++;
-                totalLength += bridge.length;
-            }
-        }
-
-        sb.append(connectedIsland == numOfIsland ? totalLength : -1);
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
     }
 
-    private static void initDist() {
-        for (int i=0; i<numOfIsland; i++) {
-            for (int j=0; j<numOfIsland; j++) {
-                dist[i][j] = Integer.MAX_VALUE;
-            }
-        }
+    static int find(int x) {
+        if (x == p[x])
+            return x;
+        return p[x] = find(p[x]);
     }
 
-    private static void updateDistance(int island) {
-        for (Point point : islandInfo.get(island)) {
-            findBridge(point, island);
-        }
-    }
-
-    private static void findBridge(Point point, int island) {
-        for (int d=0; d<4; d++) {
-            int nextX = point.x + DIR[d][0];
-            int nextY = point.y + DIR[d][1];
-            if (isInRange(nextX, nextY) && map[nextX][nextY] == EMPTY) {
-                int[] bridgeInfo = findOneWayBridge(point.x, point.y, d);
-                if (bridgeInfo[GOAL_ISLAND] != EMPTY && bridgeInfo[GOAL_ISLAND] != island && bridgeInfo[LENGTH] >= 2) {
-                    dist[island][bridgeInfo[GOAL_ISLAND]] = Math.min(dist[island][bridgeInfo[GOAL_ISLAND]], bridgeInfo[LENGTH]);
-                    dist[bridgeInfo[GOAL_ISLAND]][island] = dist[island][bridgeInfo[GOAL_ISLAND]];
-                    hasBridge[island][bridgeInfo[GOAL_ISLAND]] = true;
-                    hasBridge[bridgeInfo[GOAL_ISLAND]][island] = true;
-                }
-            }
-        }
-    }
-
-    private static int[] findOneWayBridge(int x, int y, int d) {
-        int count = 0;
-        int nextX = x + DIR[d][0];
-        int nextY = y + DIR[d][1];
-
-        while (isInRange(nextX, nextY) && map[nextX][nextY] == EMPTY) {
-            count++;
-            nextX += DIR[d][0];
-            nextY += DIR[d][1];
-        }
-
-        if (!isInRange(nextX, nextY)) {
-            return new int[]{EMPTY, EMPTY};
-        }
-        return new int[]{map[nextX][nextY], count};
-    }
-
-    private static void initParent() {
-        parent = new int[numOfIsland + 1];
-        for (int i = 1; i <= numOfIsland; i++) {
-            parent[i] = i;
-        }
-    }
-
-    private static int find(int a) {
-        if (a == parent[a]) {
-            return a;
-        }
-        return parent[a] = find(parent[a]);
-    }
-
-    private static boolean union(int a, int b) {
-        a = find(a);
-        b = find(b);
-
-        if (a == b) return false;
-        if (a < b) {
-            parent[b] = a;
+    static boolean union(int x, int y) {
+        int px = find(x);
+        int py = find(y);
+        if (px == py)
+            return false;
+        if (rank[px] >= rank[py]) {
+            rank[px]++;
+            p[py] = px;
         } else {
-            parent[a] = b;
+            rank[py]++;
+            p[px] = py;
         }
         return true;
     }
 
-    private static void bfs(int i, int j, int numOfIsland) {
-        Deque<Point> q = new ArrayDeque<>();
-        q.add(new Point(i, j));
-        map[i][j] = numOfIsland;
-        islandInfo.get(numOfIsland).add(new Point(i, j));
-
-        while (!q.isEmpty()) {
-            Point now = q.poll();
-
-            for (int[] d : DIR) {
-                int nextX = now.x + d[0];
-                int nextY = now.y + d[1];
-                if (isInRange(nextX, nextY) && map[nextX][nextY] == ISLAND) {
-                    map[nextX][nextY] = numOfIsland;
-                    islandInfo.get(numOfIsland).add(new Point(nextX, nextY));
-                    q.add(new Point(nextX, nextY));
-                }
-            }
-        }
-    }
-
-    private static boolean isInRange(int x, int y) {
-        return x >= 0 && y >= 0 && x < N && y < M;
-    }
-
-    static class Bridge implements Comparable<Bridge> {
-        int v1, v2;
-        int length;
-
-        public Bridge(int v1, int v2) {
-            this.v1 = v1;
-            this.v2 = v2;
-            this.length = dist[v1][v2];
-        }
-
-        @Override
-        public int compareTo(Bridge o) {
-            return this.length - o.length;
-        }
-    }
-
-
-    static class Point {
-        int x, y;
-
-        public Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
 }
